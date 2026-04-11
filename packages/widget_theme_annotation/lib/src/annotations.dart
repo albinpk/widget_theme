fb6963db035a202a3d5d147ee3af7cf12e6d7d5e
Albin <56157868+albinpk@users.noreply.github.com>
2026-04-11 20:15:39 +0530

    docs: add doc comment for annotation

--- packages/widget_theme_annotation/lib/src/annotations.dart
+++ packages/widget_theme_annotation/lib/src/annotations.dart
@@ -1,8 +1,11 @@
 import 'package:meta/meta_meta.dart';
 
+/// Annotation used to trigger code generation for a widget theme.
 @Target({.classType})
 class WidgetTheme {
+  /// Creates a [WidgetTheme] annotation.
   const WidgetTheme();
 }
 
+/// Annotation instance of [WidgetTheme].
 const widgetTheme = WidgetTheme();
